dbf8f0b7baaeebfe3a79ebaa2210cfa9d2447852
Jangtaejin <jtjisgod@gmail.com>
2018-09-12 02:04:47 +0900

    Some dirty code deleted in app.py
    .

--- app.py
+++ app.py
@@ -2,9 +2,6 @@
 
 # Python Utils
 import JThread      # Simple Thread Module
-import JTest        # Time Test Module
-import JDomain      # Domain Object
-import JFIO         # File I/O Helper
 import JCommand     # For TUI Mode
 import JGui         # For Web Mode
 
@@ -15,17 +12,9 @@ import JDns         # DNS Digging tool
 from JEnum import JEnum
 
 def main() :
-    interactiveMode()
-    domains = JFIO.readFile2Line(JEnum.domainFile)
-    mJDomain = JDomain.JDomain(domains)
     mJCommand = JCommand.JCommand()
     while mJCommand.control(input("JPen >> ")) :
         pass
-    
-
-def interactiveMode() :
-    domain = input("domain file? [default=domains.txt] >> ")
-    JEnum.domainFile = domain if domain else JEnum.domainFile
 
 if __name__ == "__main__" :
     main()
